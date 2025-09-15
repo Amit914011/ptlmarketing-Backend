df58d1d3c43aa0830f01ac7d6fbbf3e5e95a5167
Amit914011 <amitmrj914011@gmail.com>
2025-09-15 12:48:25 +0530

    Add cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ if (!fs.existsSync(uploadDir)) {
 const app=express()
 const PORT=process.env.PORT||8080
 app.use(express.json())
-app.use(cors())
+app.use(cors({credentials: true}))
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 // Database Connection Here 
